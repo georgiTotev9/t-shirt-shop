e0d207c26451bc52c509799cef96c2cb40cfde3d
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-01-24 21:56:34 +0200

    little fix

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,11 +1,14 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import { useEffect } from 'react';
 
 const Logout = () => {
     const { logout } = useAuth();
-    (async () => {
-        await logout();
-    })();
+    useEffect(() => {
+        (async () => {
+            await logout();
+        })();
+    }, [logout]);
 
     return <Navigate to='/' replace={true} />;
 };
