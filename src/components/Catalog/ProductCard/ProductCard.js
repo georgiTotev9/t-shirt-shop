087fd889fb64400669a6e29d7e9d7c1678f4acfa
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-01-30 23:08:26 +0200

    added catalog component and product card

---
+++ src/components/Catalog/ProductCard/ProductCard.js
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+
+const ProductCard = ({ product }) => {
+    return (
+        <div className='col-md-4'>
+            <div className='card mb-4 product-wap rounded-0'>
+                <div className='card rounded-0'>
+                    <img
+                        className='card-img rounded-0 img-fluid'
+                        src={product.imageUrl}
+                        alt=''
+                    />
+                    <div className='card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center'>
+                        <Link to={`/details/${product.id}`}></Link>
+                    </div>
+                </div>
+                <div className='card-body'>
+                    <a
+                        href='shop-single.html'
+                        className='h3 text-decoration-none'>
+                        {product.name}
+                    </a>
+                    <ul className='w-100 list-unstyled d-flex justify-content-between mb-0'>
+                        <br />
+                        <li className='pt-2'>
+                            <span className='product-color-dot color-dot-red float-left rounded-circle ml-1'></span>
+                            <span className='product-color-dot color-dot-blue float-left rounded-circle ml-1'></span>
+                            <span className='product-color-dot color-dot-black float-left rounded-circle ml-1'></span>
+                            <span className='product-color-dot color-dot-light float-left rounded-circle ml-1'></span>
+                            <span className='product-color-dot color-dot-green float-left rounded-circle ml-1'></span>
+                        </li>
+                    </ul>
+                    <p className='text-center mb-0'>${product.price}.00</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
