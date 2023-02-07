577cb4713969001bb9a67e6be486ed0bd922f4b1
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-02-07 22:24:13 +0200

    added bootstrap list

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,3 +1,22 @@
-const ShoppingCart = () => {};
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const ShoppingCart = () => {
+    return (
+        <ListGroup as='ol' numbered className='shop-list'>
+            <ListGroup.Item
+                as='li'
+                className='d-flex justify-content-between align-items-start shop-list'>
+                <div className='ms-2 me-auto'>
+                    <div className='fw-bold'>Subheading</div>
+                    Cras justo odio
+                </div>
+                <Badge bg='primary' pill>
+                    14
+                </Badge>
+            </ListGroup.Item>
+        </ListGroup>
+    );
+};
 
 export default ShoppingCart;
