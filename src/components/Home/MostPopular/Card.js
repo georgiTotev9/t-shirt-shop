aca48439f39fcc796256e01a3b7ac75589e29527
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-02-18 21:25:09 +0200

    added card home component

---
+++ src/components/Home/MostPopular/Card.js
@@ -0,0 +1,36 @@
+const Card = ({ product }) => {
+    return (
+        <div className='col-12 col-md-4 mb-4'>
+            <div className='card h-100'>
+                <img
+                    src='/assets/img/mostpopular_image_1.jpg'
+                    className='card-img-top'
+                    alt='...'
+                />
+                <div className='card-body'>
+                    <ul className='list-unstyled d-flex justify-content-between'>
+                        <li>
+                            <i className='text-warning fa fa-star'></i>
+                            <i className='text-warning fa fa-star'></i>
+                            <i className='text-warning fa fa-star'></i>
+                            <i className='text-warning fa fa-star'></i>
+                            <i className='text-warning fa fa-star'></i>
+                        </li>
+                        <li className='text-muted text-right'>
+                            ${product.price}.00
+                        </li>
+                    </ul>
+                    <span className='h2 text-decoration-none text-dark'>
+                        {product.name}
+                    </span>
+                    <p className='card-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Sunt in culpa qui officia deserunt.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
